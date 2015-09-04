c813b3abc5dd10d4b9a38e88189fafcb69af86af
Christopher Hiller <boneskull@boneskull.com>
2015-09-04 13:47:26 -0700

    add fishy code

---
+++ fishy/README.md
@@ -0,0 +1,3 @@
+# fishy
+
+ATtiny85 NeoPixel controller; does a simple, slow rainbow.  I use only one NeoPixel @ 5V.  I put this in my fish tank and turn it on at night.
---
+++ fishy/fishy.ino
@@ -0,0 +1,56 @@
+// NeoPixel Ring simple sketch (c) 2013 Shae Erisson
+// released under the GPLv3 license to match the rest of the AdaFruit NeoPixel library
+
+#include <Adafruit_NeoPixel.h>
+#include <avr/power.h>
+
+// Which pin on the Arduino is connected to the NeoPixels?
+// On a Trinket or Gemma we suggest changing this to 1
+#define PIN            1
+
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS      1
+
+// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
+// Note that for older NeoPixel pixelss you might need to change the third parameter--see the strandtest
+// example for more information on possible values.
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if (WheelPos < 85) {
+    return pixels.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else if (WheelPos < 170) {
+    WheelPos -= 85;
+    return pixels.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  } else {
+    WheelPos -= 170;
+    return pixels.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  }
+}
+
+int delayval = 500; // delay for half a second
+
+void setup() {
+  pixels.begin(); // This initializes the NeoPixel library.
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for (j = 0; j < 256 * 5; j++) { // 5 cycles of all colors on wheel
+    for (i = 0; i < pixels.numPixels(); i++) {
+      pixels.setPixelColor(i, Wheel(((i * 256 / pixels.numPixels()) + j) & 255));
+    }
+    pixels.show();
+    delay(wait);
+  }
+}
+
+void loop() {
+
+  rainbowCycle(1000);
+}
